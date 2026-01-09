a8bf3634fec47225f0a20e3184d923e5a8bbc89d
olafkruczek <128062377+olafkruczek@users.noreply.github.com>
2026-01-09 01:46:43 +0100

    Update API_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ---- Konfiguration ----
 // Trage hier die URL deiner deployten Google Apps Script Web-App ein.
 // Beispiel: "https://script.google.com/macros/s/AKfy.../exec"
-const API_URL = "https://script.google.com/macros/s/AKfycbzP8sQWzit2UeVCtDIn3pyYBV6vfCdBPgqIGk_0PFWTZyCILdaZwbsEJrkyzhEJMYcM/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxBxOv2gnk8Q4ZfjYlFOjnzb-BeFvH5I5XBWXwVd50aXL8fsIFUkHhhjI64tuUvXTbg/exec";
 
 // Escape Rooms (statisch, wie von dir gewünscht)
 const ROOMS = [
